57b0b7c26b6935d39ec1db09ff984d83fa320796
dellyu03 <seunghwan081@gmail.com>
2023-01-23 20:43:09 +0900

    정리

--- 실습 및 연습/index.js
+++ 실습 및 연습/index.js
@@ -1,8 +1,13 @@
-var cart = {name : '도서', price : 1500};
-var getTotal = function(cart){
-    return `${cart.price}원`
+var array1 = ['one', 'tow'];
+var array2 = ['three', 'four'];
+var combined = [array1[0], array1[1], array2[0], array2[1]];
+var combined = array1.concat(array2); // concat 함수 배열이나 문자열을 합치는 함수 array1과 array2를 합침
+var combined = [].concat(array1, array2); // 빈 배열에 array1과 array를 합침
+var first = array1[0];
+var second = array1[1];
+var three = array1[2] || 'empty';
+function func(){
+    var args = Array.prototype.slice.call(this, arguments);
+    var first = args[0];
+    var others = args.slice(1, args.length);
 }
-
-var myCart = `장바구니에 ${cart.name}가 있습니다. 총 금액은 ${getTotal(cart)}입니다.`
-
-console.log(myCart);
