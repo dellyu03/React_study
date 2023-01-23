e43a6e9737449a4ccc0508559f14a8258453a49e
dellyu03 <seunghwan081@gmail.com>
2023-01-23 23:30:08 +0900

    전개 연산자, 클래스

--- 실습 및 연습/index.js
+++ 실습 및 연습/index.js
@@ -1,13 +1,5 @@
-var array1 = ['one', 'tow'];
-var array2 = ['three', 'four'];
-var combined = [array1[0], array1[1], array2[0], array2[1]];
-var combined = array1.concat(array2); // concat 함수 배열이나 문자열을 합치는 함수 array1과 array2를 합침
-var combined = [].concat(array1, array2); // 빈 배열에 array1과 array를 합침
-var first = array1[0];
-var second = array1[1];
-var three = array1[2] || 'empty';
-function func(){
-    var args = Array.prototype.slice.call(this, arguments);
-    var first = args[0];
-    var others = args.slice(1, args.length);
-}
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9];
+const arrWrap = arr1.concat(arr2);
+
+console.log(arrWrap);
